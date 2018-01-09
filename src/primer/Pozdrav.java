6382ff73c5ac3477563bb5c8d2135bee31b4f632
Natasa Sukur <nts@dmi.uns.ac.rs>
2018-01-09 18:06:33 +0100

    Obrisan uzvicnik

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -4,7 +4,7 @@ public class Pozdrav {
 
 	public static void main(String[] args) {
 		System.out.println("Zdravo, Natasa Sukur!");
-		System.out.println("Zdravo i tebi!");
+		System.out.println("Zdravo i tebi");
 	}
 
 }
