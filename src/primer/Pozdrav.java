e7c554284cba1a0081b9339f11ccad311603a0ff
Natasa Sukur <nts@dmi.uns.ac.rs>
2018-01-09 18:09:50 +0100

    Obrisan uzvicnik, popravljeno za merge

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -3,12 +3,7 @@ package primer;
 public class Pozdrav {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Zdravo i tebi!");
-=======
-		System.out.println("Zdravo, Natasa Sukur!");
 		System.out.println("Zdravo i tebi");
->>>>>>> refs/heads/natasa-sukur
 	}
 
 }
