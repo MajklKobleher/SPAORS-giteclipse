a1d8fc2448e5c0f9305dc02da5a772881c736336
MajklKobleher <dmi.mihael.kobleher@student.pmf.uns.ac.rs>
2018-01-17 11:43:02 +0100

    Zdravo

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -3,8 +3,7 @@ package primer;
 public class Pozdrav {
 
 	public static void main(String[] args) {
-		System.out.println("Zdravo i tebi");
-		System.out.println("aaaaaaa");
+		System.out.println("Zdravo");
 	}
 
 }
